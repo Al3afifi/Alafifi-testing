e27d49e6b863add797dcfd8e9794f2cebdfdfa3b
Hamood <mumualafifi@gmail.com>
2022-11-01 11:01:21 -0400

    fixed read write submit test app

--- AlafifiPractice/firebase-login/index.js
+++ AlafifiPractice/firebase-login/index.js
@@ -1,71 +1,68 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyC8FdNrAxNMazFcCgeTlbCZ_xrSw6JoRwc",
-    authDomain: "testing-password-with-info.firebaseapp.com",
-    databaseURL: "https://testing-password-with-info-default-rtdb.firebaseio.com",
-    projectId: "testing-password-with-info",
-    storageBucket: "testing-password-with-info.appspot.com",
-    messagingSenderId: "106238843238",
-    appId: "1:106238843238:web:906d27d528380a09bb90a7"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  // Set database variable
-  var database = firebase.database()
-  
-  function save() {
-    console.log("sup");
-    var email = document.getElementById('email').value
-    var password = document.getElementById('password').value
-    var username = document.getElementById('username').value
-    var say_something = document.getElementById('say_something').value
-    var favourite_food = document.getElementById('favourite_food').value
-  
-    database.ref('users/' + username).set({
-      email : email,
-      password : password,
-      username : username,
-      say_something : say_something,
-      favourite_food : favourite_food
-    })
-  
-    alert('Saved')
+  apiKey: "AIzaSyC8FdNrAxNMazFcCgeTlbCZ_xrSw6JoRwc",
+  authDomain: "testing-password-with-info.firebaseapp.com",
+  projectId: "testing-password-with-info",
+  storageBucket: "testing-password-with-info.appspot.com",
+  messagingSenderId: "106238843238",
+  appId: "1:106238843238:web:906d27d528380a09bb90a7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Set database variable
+var database = firebase.database()
+
+function save() {
+  var email = document.getElementById('email').value
+  var password = document.getElementById('password').value
+  var username = document.getElementById('username').value
+  var say_something = document.getElementById('say_something').value
+  var favourite_food = document.getElementById('favourite_food').value
+
+  database.ref('users/' + username).set({
+    email : email,
+    password : password,
+    username : username,
+    say_something : say_something,
+    favourite_food : favourite_food
+  })
+
+  alert('Saved')
+}
+
+function get() {
+  var username = document.getElementById('username').value
+
+  var user_ref = database.ref('users/' + username)
+  user_ref.on('value', function(snapshot) {
+    var data = snapshot.val()
+
+    alert(data.email)
+
+  })
+
+}
+
+function update() {
+  var username = document.getElementById('username').value
+  var email = document.getElementById('email').value
+  var password = document.getElementById('password').value
+
+  var updates = {
+    email : email,
+    password : password
   }
-  
-  function get() {
-    var username = document.getElementById('username').value
-  
-    var user_ref = database.ref('users/' + username)
-    user_ref.on('value', function(snapshot) {
-      var data = snapshot.val()
-  
-      alert(data.email)
-  
-    })
-  
-  }
-  
-  function update() {
-    var username = document.getElementById('username').value
-    var email = document.getElementById('email').value
-    var password = document.getElementById('password').value
-  
-    var updates = {
-      email : email,
-      password : password
-    }
-  
-    database.ref('users/' + username).update(updates)
-  
-    alert('updated')
-  }
-  
-  function remove() {
-    var username = document.getElementById('username').value
-  
-    database.ref('users/' + username).remove()
-  
-    alert('deleted')
-  }
-  
+
+  database.ref('users/' + username).update(updates)
+
+  alert('updated')
+}
+
+function remove() {
+  var username = document.getElementById('username').value
+
+  database.ref('users/' + username).remove()
+
+  alert('deleted')
+}
